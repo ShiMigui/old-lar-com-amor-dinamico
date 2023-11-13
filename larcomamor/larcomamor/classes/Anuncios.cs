9a60bd925525df0a70bd5a7a905ee011f8609dfb
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2023-11-13 13:39:50 -0300

    update banco e anuncios

--- larcomamor/larcomamor/classes/Anuncios.cs
+++ larcomamor/larcomamor/classes/Anuncios.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace larcomamor.classes
 {
-    public class Anuncios
+    public class Anuncios : Banco
     {
-
-
+        public static string ComandoAnimais(string txt = "")
+        {
+            string command = $@"SELECT * FROM animal a 
+                WHERE (a.nm_animal LIKE '%{txt}%' OR a.ds_animal LIKE '%{txt}%')";
+            return command;
+        }
     }
 }
---
+++ larcomamor/larcomamor/classes/Banco.cs
@@ -0,0 +1,106 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace larcomamor.classes
+{
+    public class Banco
+    {
+        private MySqlConnection conexao = null;
+        private MySqlDataReader data = null;
+
+        public Banco()
+        {
+            this.conexao = new MySqlConnection(Credencial.Conexao());
+        }
+
+        public void Abrir()
+        {
+            Fechar();
+            conexao.Open();
+        }
+
+        public void Fechar()
+        {
+            if (conexao != null) if (conexao.State == System.Data.ConnectionState.Open) conexao.Close();
+        }
+
+        public void Executar(string comando)
+        {
+            try
+            {
+                Abrir();
+                using (MySqlCommand cmd = new MySqlCommand(comando, conexao))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                throw new Exception("Não foi possível conectar-se ao banco de dados");
+            }
+        }
+
+        public void Executar(string comando, List<ListItem> paramsCmd)
+        {
+            try
+            {
+                Abrir();
+                using (MySqlCommand cmd = AdicionarParametros(new MySqlCommand(comando, conexao), paramsCmd))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                throw new Exception("Não foi possível conectar-se ao banco de dados");
+            }
+        }
+
+        public MySqlDataReader Consultar(string comando)
+        {
+            try
+            {
+                Abrir();
+                MySqlCommand cmd = new MySqlCommand(comando, conexao);
+                data = cmd.ExecuteReader();
+                return data;
+            }
+            catch
+            {
+                throw new Exception("Não foi possível conectar-se ao banco de dados");
+            }
+        }
+
+        public MySqlDataReader Consultar(string comando, List<ListItem> paramsCmd)
+        {
+            try
+            {
+                Abrir();
+                MySqlCommand cmd = AdicionarParametros(new MySqlCommand(comando, conexao), paramsCmd);
+                data = cmd.ExecuteReader();
+                return data;
+            }
+            catch
+            {
+                throw new Exception("Não foi possível conectar-se ao banco de dados");
+            }
+        }
+
+        private MySqlCommand AdicionarParametros(MySqlCommand cmd, List<ListItem> paramsCmd)
+        {
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            if (paramsCmd == null || paramsCmd.Count <= 0) return cmd;
+            foreach (ListItem param in paramsCmd)
+            {
+                if (param != null)
+                {
+                    cmd.Parameters.AddWithValue(param.Text, param.Value);
+                }
+            }
+            return cmd;
+        }
+    }
+}
