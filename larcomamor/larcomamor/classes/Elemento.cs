5dbfbb8809bd5cb9175876087d3eccf5cab539af
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2023-11-13 12:28:49 -0300

    update

---
+++ larcomamor/larcomamor/classes/Elemento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace larcomamor.classes
+{
+    public class Elemento
+    {
+
+
+        static public string AnuncioAnimal()
+        {
+            /*nm_animal,cd_animal,nm_idade*/
+            return $@"<a href='./animal.aspx?cd=cd_animal' cd='cd_animal' sg='a' class='animalAnuncio'><div class='images'>{FotoAnuncioAnimal()}</div><div class='texts'><h4>nm_animal</h4><p>nm_idade</p><p class='link textEnd'>ver mais</p></div></a>";
+        }
+
+        static public string FotoAnuncioAnimal()
+        {
+            return "<img class='animalPhoto' src='./img/animal/cd_animal.jpg' alt='imagem do animal nm_animal'>";
+        }
+
+        static public string FotoAnuncioOrganizacao()
+        {
+            return "<img class='organizacaoPhoto' src='./img/usuario/cd_usuario.jpg' alt='imagem de nm_usuario'>";
+        }
+
+        static public string FotoAnuncioEvento()
+        {
+            return "<img class='eventoPhoto' src='./img/usuario/cd_evento.jpg' alt='imagem do evento nm_evento'>";
+        }
+    }
+}
