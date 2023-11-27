c7fefd1898846948c92a6484e0e0055c87e4a059
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2023-11-27 05:10:23 -0300

    update

---
+++ lar_com_amor/lar_com_amor/Eventos.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Eventos.aspx.cs" Inherits="lar_com_amor.Eventos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Lar com amor | Eventos</title>
+<link rel="stylesheet" href="../style/global.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+<link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&amp;display=swap" rel="stylesheet"> 
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header>
+            <section class="limiter flex alignCenter justBetween">
+                <a href="./index.aspx">
+                    <img src="./img/icons/logo.png" class="logo" alt="Logotipo Lar com amor"></a>
+
+                <section id="pesquisa">
+                    <input type="text" class="input">
+                    <input type="image" src="./img/icons/search.png" alt="Ícone de pesquisa">
+                </section>
+
+                <section id="campo-navegacao" class="flex justEnd alignCenter">
+                    <nav ativo="false">
+                        <a href="./animais.aspx">Animais</a>
+                        <a href="./eventos.aspx">Eventos</a>
+                        <a href="./organizacoes.aspx">Organizações</a>
+                    </nav>
+
+                    <section id="menu-hamburguer" class="flexColumn square" ativo="false">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </section>
+
+                    <asp:Literal Text="" runat="server" ID="litHeader" />
+                </section>
+            </section>
+        </header>
+
+        <main class="limiter">
+            <article class="content-box filtros" id="campo-pesquisa">
+                <div class="input-line">
+                        <asp:TextBox ID="txtPesquisa" runat="server" CssClass="input-line-100" type="text" ></asp:TextBox>
+                    <input type="image" src="./img/icons/search.png" alt="Ícone de lupa para pesquisa">
+                </div>
+                <div class="grid4">
+                    <div class="itemForm">
+                        <label for="selTipo">Tipo de evento</label>
+                        <asp:DropDownList ID="ddlTipoEvento" runat="server" CssClass="itemForm" AutoPostBack="true"></asp:DropDownList>
+                    </div>
+                    <div class="itemForm">
+                        <label for="selEstado">Estado</label>
+                        <asp:DropDownList ID="ddlEstado" runat="server" CssClass="itemForm" AutoPostBack="true"></asp:DropDownList>
+                    </div>
+                    <div class="itemForm">
+                        <label for="selCidade">Cidade</label>
+                        <asp:DropDownList ID="ddlCidade" runat="server" AutoPostBack="true"></asp:DropDownList>
+                    </div>
+                </div>
+            </article>
+
+             <asp:Literal ID="litEventos" runat="server"></asp:Literal>
+        </main>
+    </form>
+</body>
+</html>
---
+++ lar_com_amor/lar_com_amor/Eventos.aspx.cs
@@ -0,0 +1,153 @@
+﻿using lar_com_amor.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lar_com_amor
+{
+    public partial class Eventos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Parametro Pegar = new Parametro();
+            List<Parametro> lista = new List<Parametro>();
+            lista = Pegar.PegarEstado();
+
+            if (!IsPostBack)
+            {
+                Usuario usuario = new Usuario();
+                usuario.HeaderContent(litHeader);
+
+                ddlEstado.Items.Add(new ListItem("Selecione", "0"));
+                foreach (Parametro parametro in lista)
+                {
+                    ddlEstado.Items.Add(new ListItem(parametro.Nm, parametro.Vl));
+                }
+
+                lista = Pegar.PegarTipoEvento();
+                ddlTipoEvento.Items.Add(new ListItem("Selecione", "0"));
+                foreach (Parametro parametro in lista)
+                {
+                    ddlTipoEvento.Items.Add(new ListItem(parametro.Nm, parametro.Vl));
+                }
+
+               
+            }
+
+            if (ddlEstado.SelectedValue == "0")
+            {
+                ddlCidade.Enabled = false;
+            }
+            else
+            {
+                string cd_cidade = ddlCidade.SelectedValue;
+                ddlCidade.Enabled = true;
+                ddlCidade.Items.Clear();
+                lista = Pegar.PegarCidade(ddlEstado.SelectedValue.ToString());
+                ddlCidade.Items.Add(new ListItem("Selecione", "0"));
+                foreach (Parametro parametro in lista)
+                {
+                    ddlCidade.Items.Add(new ListItem(parametro.Nm, parametro.Vl));
+                }
+                try
+                {
+                    ddlCidade.SelectedValue = cd_cidade;
+                }
+                catch
+                {
+                    ddlCidade.SelectedValue = "0";
+                }
+            }
+
+            Pesquisa();
+        }
+
+        public void Pesquisa()
+        {
+            Anuncios anuncios = new Anuncios();
+            if (String.IsNullOrEmpty(txtPesquisa.Text))
+            {
+                if (ddlTipoEvento.SelectedValue == "0")
+                {
+                    if(ddlEstado.SelectedValue != "0")
+                    {
+                        if(ddlCidade.SelectedValue != "0")
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos("", "", "","0","4",true,ddlEstado.SelectedValue.ToString(),ddlCidade.SelectedValue.ToString()), message: "Organizações não encontradas");
+                        }
+                        else
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos("", "", "", "0", "4", true, ddlEstado.SelectedValue.ToString(), ""), message: "Organizações não encontradas");
+                        }
+                    }
+                    else
+                    {
+                        litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(), message: "Organizações não encontradas");
+                    }
+                    
+                }
+                else
+                {
+                    if (ddlEstado.SelectedValue != "0")
+                    {
+                        if (ddlCidade.SelectedValue != "0")
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos("", "", ddlTipoEvento.SelectedValue.ToString(), "0", "4", true, ddlEstado.SelectedValue.ToString(), ddlCidade.SelectedValue.ToString()), message: "Organizações não encontradas");
+                        }
+                        else
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos("", "", ddlTipoEvento.SelectedValue.ToString(), "0", "4", true, ddlEstado.SelectedValue.ToString(), ""), message: "Organizações não encontradas");
+                        }
+                    }
+                    else
+                    {
+                        litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos("","", ddlTipoEvento.SelectedValue.ToString()), message: "Organizações não encontradas");
+                    }
+                }                
+            }
+            else
+            {
+                if (ddlTipoEvento.SelectedValue == "0")
+                {
+                    if (ddlEstado.SelectedValue != "0")
+                    {
+                        if (ddlCidade.SelectedValue != "0")
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(txtPesquisa.Text.ToString(), "", "", "0", "4", true, ddlEstado.SelectedValue.ToString(), ddlCidade.SelectedValue.ToString()), message: "Organizações não encontradas");
+                        }
+                        else
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(txtPesquisa.Text.ToString(), "", "", "0", "4", true, ddlEstado.SelectedValue.ToString(), ""), message: "Organizações não encontradas");
+                        }
+                    }
+                    else
+                    {
+                        litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(txtPesquisa.Text.ToString()), message: "Organizações não encontradas");
+                    }
+
+                }
+                else
+                {
+                    if (ddlEstado.SelectedValue != "0")
+                    {
+                        if (ddlCidade.SelectedValue != "0")
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(txtPesquisa.Text.ToString(), "", ddlTipoEvento.SelectedValue.ToString(), "0", "4", true, ddlEstado.SelectedValue.ToString(), ddlCidade.SelectedValue.ToString()), message: "Organizações não encontradas");
+                        }
+                        else
+                        {
+                            litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(txtPesquisa.Text.ToString(), "", ddlTipoEvento.SelectedValue.ToString(), "0", "4", true, ddlEstado.SelectedValue.ToString(), ""), message: "Organizações não encontradas");
+                        }
+                    }
+                    else
+                    {
+                        litEventos.Text = Elemento.GerarAnuncios(anuncios.GetEventos(txtPesquisa.Text.ToString(), "", ddlTipoEvento.SelectedValue.ToString()), message: "Organizações não encontradas");
+                    }
+                }
+            }
+        }
+    }
+}
